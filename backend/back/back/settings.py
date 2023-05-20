377dd1aa2b44c38fb22a0f3815d52b25b9c520fc
Nikita Titarenko <ntv.cnd@gmail.com>
2023-05-20 12:42:19 +0300

    again

--- backend/back/back/settings.py
+++ backend/back/back/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=$ay2%e@e*w+%_wp-qkdh7pa_g_bv1wv1goct5!&&(1pi@hez-'
+SECRET_KEY = 'django-insecure-$c6-sazvw99n6$-baib^syvqrch=oeuuqy0mbmhdf3-tj)191b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,15 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
-    'recipes.apps.RecipesConfig',
-    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
