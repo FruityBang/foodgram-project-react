d997ee37a21afbe55b3e7a9abd965ddbeccfc4cf
Nikita Titarenko <ntv.cnd@gmail.com>
2023-05-25 18:27:19 +0300

    here we go again

--- backend/back/users/admin.py
+++ backend/back/users/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 
-from .models import User
+from .models import Subscribe, User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username', 'pk', 'email', 'password', 'first_name', 'last_name',
+    )
+    list_editable = ('password', )
+    list_filter = ('username', 'email')
+    search_fields = ('username', 'email')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'author')
+    list_editable = ('user', 'author')
+    empty_value_display = '-пусто-'
