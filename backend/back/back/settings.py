24d45ec60a89885aa6a9c5c9be5c51c807369ae2
Nikita Titarenko <ntv.cnd@gmail.com>
2023-05-25 15:45:03 +0300

    recipe_app_added

--- backend/back/back/settings.py
+++ backend/back/back/settings.py
@@ -9,8 +9,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    'api.apps.ApiConfig',
     'users.apps.UsersConfig',
+    'recipes.apps.RecipesConfig',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
