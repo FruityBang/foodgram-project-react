3d2640cbaad30950dd55497002a8998f5c05d265
Nikita Titarenko <ntv.cnd@gmail.com>
2023-05-30 19:18:18 +0300

    2

--- backend/back/users/admin.py
+++ backend/back/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk', 'username', 'email', 'first_name', 'last_name', 'password'
+    )
+    list_editable = ('password', )
+    list_filter = ('username', 'email')
+    search_fields = ('username', 'email')
+
+
+@admin.register(models.Follow)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk', 'user', 'author'
+    )
+    list_editable = ('user', 'author')
